f732b98990894b9fa0d2b9db431fd04de6790fa4
fragata-adrian <fragataadrian99@gmail.com>
2020-12-02 00:02:18 -0300

    Muestra el tiempo actual en hurlingham

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import Formulario from './components/Formulario';
@@ -7,22 +7,22 @@ import Pronostico from './components/Pronostico';
 
 function App() {
 
-  // State para guardar los objetos resultantes de la consulta
+  // State para mostrar el clima o no
+  const [consulta, setConsulta] = useState(false);
 
+  // State para guardar los objetos resultantes de la consulta
   const [clima, setClima] = useState({});
 
-  // Extraigo los valores que voy a utilizar
-
-  // Funcion para consultar a la API sp, es
-
+  // Funcion para consultar a la API
   const consultarApi = async () => {
     try {
-      const respuesta = await fetch('api.openweathermap.org/data/2.5/weather?id=3433955&appid=2463ce6ff06814eefef00152d963284a&units=metric&lang=es');
+      const respuesta = await fetch('http://api.openweathermap.org/data/2.5/weather?q=hurlingham&appid=2463ce6ff06814eefef00152d963284a&mode=json&lang=es');
       const datos = await respuesta.json();
-      console.log(datos);
+      setClima(datos);
     } catch (error) {
       console.log(error);
     }
+    setConsulta(true);
   }
 
   return (
@@ -43,15 +43,12 @@ function App() {
 
           <Row>
             <Col>
-              <Pronostico 
-                /*icon = {clima['weather'][0].icon}
-                description = {description}
-                temp = {temp}
-                temp_min = {temp_min}
-                temp_max = {temp_max}
-                humidity = {humidity}
-                name = {name}*/
-              />
+              {consulta
+                ? <Pronostico 
+                    clima = {clima} 
+                  />
+                : <h3>Ingrese una ciudad para consultar</h3>
+              }
             </Col>
           </Row>
         </Container>
--- src/components/Pronostico.jsx
+++ src/components/Pronostico.jsx
@@ -1,38 +1,23 @@
 import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Image } from 'react-bootstrap';
-import img01d from '../images/01d.png';
-import img01n from '../images/01d.png';
-import img02d from '../images/01d.png';
-import img02n from '../images/01d.png';
-import img03d from '../images/01d.png';
-import img03n from '../images/01d.png';
-import img04d from '../images/01d.png';
-import img04n from '../images/01d.png';
-import img09d from '../images/01d.png';
-import img09n from '../images/01d.png';
-import img10d from '../images/01d.png';
-import img10n from '../images/01d.png';
-import img11d from '../images/01d.png';
-import img11n from '../images/01d.png';
-import img13d from '../images/01d.png';
-import img13n from '../images/01d.png';
-import img50d from '../images/01d.png';
-import img50n from '../images/01d.png';
 
-const Pronostico = ({icon, description, temp, temp_min, temp_max, humidity, name}) => {
+const Pronostico = ({ clima }) => {
 
-  // Armo la ruta para ir a buscar la imagen correspondiente
-  const image = "img"+icon;
-
+  // Extraigo los valores que necesito
+  const weather = clima.weather[0];
+  const { icon, description } = weather;
+  const { temp, temp_min, temp_max, humidity } = clima.main;
+  const imagen = 'http://openweathermap.org/img/wn/'+ icon +'@2x.png';
+  
   return (
     <Fragment>
-        <h3>Tiempo en {name}</h3>
-        <Image src={image} rounded />
-        <p>{temp}</p>
-        <p>{description}</p>
-        <p>min {temp_min} - max {temp_max} </p>
-        <p>{humidity}</p>
+      <h3>Tiempo en {clima.name}</h3>
+      <Image src={imagen} rounded />
+      <p>{(temp - 273.15).toFixed(1)}°</p>
+      <p>{description}</p>
+      <p>min {(temp_min - 273.15).toFixed(1)} - max {(temp_max - 273.15).toFixed(1)}</p>
+      <p>Humedad {humidity}%</p>
     </Fragment>
   );
 }
